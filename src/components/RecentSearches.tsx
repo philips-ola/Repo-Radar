8598305ef5003c3cad58495019456fea3b458b76
Ola Philips. <olaphilips310@gmail.com>
2026-09-13 03:04:16 +0100

    Update RecentSearches.tsx

--- src/components/RecentSearches.tsx
+++ src/components/RecentSearches.tsx
@@ -1,39 +1,42 @@
-
 import { FaClock } from "react-icons/fa";
 import { useQueryClient } from "@tanstack/react-query";
-import { fetchGithubUser } from "../api/Github";
+import { fetchGithubUser } from "../api/github";
 
-export const RecentSearches = ({recentUsers, setUsername, setSubmittedUsername}) => {
+type RecentSearchesProps = {
+  recentUsers: string[]
+  setUsername: (value: string) => void
+  setSubmittedUsername: (value: string) => void
+}
 
-    const queryClient = useQueryClient();
+export const RecentSearches = ({ recentUsers, setUsername, setSubmittedUsername }: RecentSearchesProps) => {
+  const queryClient = useQueryClient();
 
-    return <>
-                <div className="recent-searches">
-            <div className="recent-header">
-            <FaClock />
-            <h3>Recent Searches</h3>
-            </div>
-            <ul>
-            {recentUsers.map((user) => (
-                <li key={user}>
-                <button
-                    onClick={() => {
-                    setUsername(user);
-                    setSubmittedUsername(user);
-                    }}
-                    onMouseEnter={() => 
-                        {queryClient.prefetchQuery({
-                            queryKey: ['users', user],
-                            queryFn: () => fetchGithubUser(user)
-                        })
-                    }
-                    }
-                >
-                    {user}
-                </button>
-                </li>
-            ))}
-            </ul>
-        </div>
-    </>
+  return (
+    <div className="recent-searches">
+      <div className="recent-header">
+        <FaClock />
+        <h3>Recent Searches</h3>
+      </div>
+      <ul>
+        {recentUsers.map((user: string) => (
+          <li key={user}>
+            <button
+              onClick={() => {
+                setUsername(user);
+                setSubmittedUsername(user);
+              }}
+              onMouseEnter={() => {
+                queryClient.prefetchQuery({
+                  queryKey: ['users', user],
+                  queryFn: () => fetchGithubUser(user),
+                })
+              }}
+            >
+              {user}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
